109f6fc33d43e0137922b809dfb892405703693a
melshabazz2 <melshabazz2@gmail.com>
2025-10-30 11:56:13 -0400

    completed assignment

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,22 +1,46 @@
 // Create callback functions for Higher-Order Functions:
-const logEachValue = () => { };
+const logEachValue = (arr) => {
+  arr.forEach((value, index) => {
+    console.log(`Value: ${value}, index: ${index}.`)
+  })
+};
 
-const makePeopleHappy = () => { };
 
-const getEvenNumbers = () => { };
+const makePeopleHappy = (arr) => {
+  arr.forEach((people) => people.isHappy = true)
+};
 
-const doubleEveryNumber = () => { };
+const getEvenNumbers = (arr) => {
+  return arr.filter((number) => number % 2 === 0)
+};
 
-const convertToBooleans = () => { };
+const doubleEveryNumber = (arr) => {
+  return arr.map((arr) => arr * 2)
+};
+
+const convertToBooleans = (arr) => {
+  // return arr.map(Boolean)
+  return arr.map(num => Boolean(num));
+};
 
 // Create your own Higher Order Functions:
-const myForEach = () => { };
+const myForEach = (arr, callback) => {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i]);
+  }
+};
 
-const myMap = () => { };
+const myMap = (array, transform) => {
+  return array.map(transform)
+};
 
-const myFilter = () => { };
+const myFilter = (array, test) => {
+  return array.filter(test)
+};
 
-const myFind = () => { };
+const myFind = (array, test) => {
+  return array.find(test)
+};
 
 // Use Array.sort() and provide the correct callback sorting function:
 module.exports = {
