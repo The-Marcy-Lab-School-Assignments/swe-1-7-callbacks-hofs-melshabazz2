d5f5fceebe70929029b0116084433870313f1113
melshabazz2 <melshabazz2@gmail.com>
2025-10-31 13:03:16 -0400

    re did fromscratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -31,15 +31,45 @@ const myForEach = (arr, callback) => {
 };
 
 const myMap = (array, transform) => {
-  return array.map(transform)
+  // create a new array
+  const newArr = []
+  // Loop through each element in the input array (original array)
+  for (let i = 0; i < array.length; i++) {
+    // apply the transform function to each element
+    const newValue = transform(array[i])
+    // add the transformed value to the new array
+    newArr.push(newValue)
+  }
+  // return the new array with the transformed value
+  return newArr
 };
 
 const myFilter = (array, test) => {
-  return array.filter(test)
+  // create a new array
+  const newArr = []
+  // loop through the elements in the input array (original array)
+  for (let i = 0; i < array.length; i++) {
+    // apply the test function to the current element
+    const value = test(array[i])
+    // if the rest returns true(or truthy), add the element to newArr
+    if (value) {
+      newArr.push(array[i])
+    }
+  }
+  return newArr
 };
 
 const myFind = (array, test) => {
-  return array.find(test)
+  // Loop through each element in the array
+  for (let i = 0; i < array.length; i++) {
+    // apply the test function to the current function
+    checkEach = test(array[i])
+    if (checkEach) {
+      return array[i]
+    }
+  }
+  return undefined
+  // return array.find(test)
 };
 
 // Use Array.sort() and provide the correct callback sorting function:
